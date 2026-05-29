1c060d42c00aea5fa6b0787ba9050ad4ba4b969a
Scott Mohekey <smohekey@insightful.co.nz>
2026-05-30 00:30:41 +1200

    Simplify generator to write table schemas

--- crates/squealy/src/generator.rs
+++ crates/squealy/src/generator.rs
@@ -1,45 +1,9 @@
 use std::io::{self, Write};
 
-use crate::{ColumnSchema, ForeignKeySchema, IndexSchema, TableSchema};
+use crate::TableSchema;
 
 /// Generates backend-specific SQL from derived table schema metadata.
 pub trait Generator {
-    /// Generate a full set of SQL statements for creating a table and its indexes.
-    fn create_table(&self, schema: &TableSchema, writer: &mut impl Write) -> io::Result<()> {
-        self.write_create_table_statement(schema, writer)?;
-        for index in schema.indexes {
-            writer.write_all(b"\n")?;
-            self.write_create_index_statement(schema, index, writer)?;
-        }
-        Ok(())
-    }
-
-    /// Generate the backend-specific `CREATE TABLE` statement.
-    fn write_create_table_statement(
-        &self,
-        schema: &TableSchema,
-        writer: &mut impl Write,
-    ) -> io::Result<()>;
-
-    /// Generate the backend-specific `CREATE INDEX` statement for one index.
-    fn write_create_index_statement(
-        &self,
-        table: &TableSchema,
-        index: &IndexSchema,
-        writer: &mut impl Write,
-    ) -> io::Result<()>;
-
-    /// Generate the backend-specific SQL fragment for one column definition.
-    fn write_column_definition(
-        &self,
-        column: &ColumnSchema,
-        writer: &mut impl Write,
-    ) -> io::Result<()>;
-
-    /// Generate the backend-specific SQL fragment for one foreign-key reference.
-    fn write_foreign_key_reference(
-        &self,
-        reference: &ForeignKeySchema,
-        writer: &mut impl Write,
-    ) -> io::Result<()>;
+    /// Generate backend-specific SQL for a table schema.
+    fn write_table(&self, schema: &TableSchema, writer: &mut impl Write) -> io::Result<()>;
 }
